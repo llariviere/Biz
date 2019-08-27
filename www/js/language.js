7c1238666ab0643bd5f3804dfe92f0b90176d1c2
Louis Larivière <louis.lariviere@gmail.com>
2019-08-27 00:00:50 -0400

    Update language.js

--- www/js/language.js
+++ www/js/language.js
@@ -1,43 +1,51 @@
-var cLANGUAGE = "fr";
-
-var languageSpecificObject = null;
+var cLANGUAGE = 'en';
+var languageSpecificObject = englishLanguageSpecificObject || null;
+/*
 var languageSpecificURL = "";
 var spanishLanguageSpecificURL = "i18n/es.json";
 var frenchLanguageSpecificURL  = "i18n/fr.json";
 var englishLanguageSpecificURL = "i18n/en.json";
+*/
 
 var languageControls = function(language){
 
 	if((language.toString() == "es") || (language.toString() == "español") || (language.toString().indexOf("es") != -1)){
-      languageSpecificURL = spanishLanguageSpecificURL;
+      //languageSpecificURL = spanishLanguageSpecificURL;
+      languageSpecificObject = spanishLanguageSpecificObject;
 	}
 	else if((language.toString() == "fr") || (language.toString() == "français") || (language.toString().indexOf("fr") != -1)){
-      languageSpecificURL = frenchLanguageSpecificURL;
+      //languageSpecificURL = frenchLanguageSpecificURL;
+      languageSpecificObject = frenchLanguageSpecificObject;
 	}
 	else{
-      languageSpecificURL = englishLanguageSpecificURL;
+      //languageSpecificURL = englishLanguageSpecificURL;
+      languageSpecificObject = englishLanguageSpecificObject;
 	}
+	
+	$$(".lshtm").each(function(){
+		$$(this).html(languageSpecificObject.languageSpecifications[0][$$(this).data("text")]);
+	});
+	$$(".lsphr").each(function(){
+		$$(this).attr("placeholder",languageSpecificObject.languageSpecifications[0][$$(this).data("text")]);
+	});
+   $$(".lsvlu").each(function(){
+		$$(this).attr("value",languageSpecificObject.languageSpecifications[0][$$(this).data("text")]);
+	});
    
    //Make an ajax call to strings.json files
-	onNetworkCall(languageSpecificURL,function(msg){
-  		languageSpecificObject = JSON.parse(msg);
-		$$(".lshtm").each(function(){
-			$$(this).html(languageSpecificObject.languageSpecifications[0][$$(this).data("text")]);
-		});
-		$$(".lsphr").each(function(){
-			$$(this).attr("placeholder",languageSpecificObject.languageSpecifications[0][$$(this).data("text")]);
-		});
-      $$(".lsvlu").each(function(){
-			$$(this).attr("value",languageSpecificObject.languageSpecifications[0][$$(this).data("text")]);
-		});
-	});
+	//onNetworkCall(languageSpecificURL,function(msg){
+  	//	languageSpecificObject = JSON.parse(msg);
+	//});
 };
 
-function lsval(key){
+var lsval = function(key){
 	value = languageSpecificObject.languageSpecifications[0][key];
 	return value;
 };
 
+languageControls(cLANGUAGE);
+
+/*
 function onNetworkCall(urlToHit,successCallback){
 	
 	$$.ajax({
@@ -51,5 +59,4 @@ function onNetworkCall(urlToHit,successCallback){
 		 },
 	});
 }
-	
-languageControls(cLANGUAGE);
+*/
