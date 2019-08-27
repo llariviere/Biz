491ede6de7cb7e50c908ceae6508289b7c868153
Louis Larivière <louis.lariviere@gmail.com>
2019-08-26 22:28:59 -0400

    Update language.js

--- www/js/language.js
+++ www/js/language.js
@@ -5,7 +5,7 @@ var languageSpecificURL = "";
 var spanishLanguageSpecificURL = "i18n/es.json";
 var frenchLanguageSpecificURL  = "i18n/fr.json";
 var englishLanguageSpecificURL = "i18n/en.json";
-
+var lsval = {};
 
 var languageControls = function(language){
 
@@ -31,12 +31,12 @@ var languageControls = function(language){
       $$(".lsvlu").each(function(){
 			$$(this).attr("value",languageSpecificObject.languageSpecifications[0][$$(this).data("text")]);
 		});
+		
+		lsval = function(key){
+			value = languageSpecificObject.languageSpecifications[0][key];
+			return value;
+		};
 	});
-};
-
-var lsval = function(key){
-	value = languageSpecificObject.languageSpecifications[0][key];
-	return value;
 };
 
 var onNetworkCall = function(urlToHit,successCallback){
